0b668da2dfb261f0100a2adb9180d8fa30f56427
Nick Gamroth <nick.gamroth@distek.com>
2017-03-05 14:48:06 -0600

    Got everything workign on android

---
+++ Laurus.Mileage/Laurus.Mileage.Droid/Services/EmailHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Android.Content;
+using Laurus.Mileage.Droid;
+using Laurus.Mileage.Services;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(EmailHelper))]
+namespace Laurus.Mileage.Droid
+{
+    public class EmailHelper : IEmailHelper
+    {
+        public EmailHelper()
+        {
+        }
+
+        public void SendEmail(string attachment)
+        {
+            var email = new Intent(Android.Content.Intent.ActionSend);
+            //email.PutExtra(Android.Content.Intent.ExtraEmail,
+            //    new string[] { "person1@xamarin.com", "person2@xamrin.com" });
+
+            //email.PutExtra(Android.Content.Intent.ExtraCc,
+            //new string[] { "person3@xamarin.com" });
+
+            email.PutExtra(Android.Content.Intent.ExtraSubject, "Expense Report");
+
+            email.PutExtra(Android.Content.Intent.ExtraText, "Expense report from MileageApp");
+            var file = new Java.IO.File(attachment);
+            file.SetReadable(true, true);
+            var uri = Android.Net.Uri.FromFile(file);
+            email.PutExtra(Intent.ExtraStream, uri);
+
+            email.SetType("message/rfc822");
+
+
+            Forms.Context.StartActivity(Intent.CreateChooser(email, "Send email"));
+        }
+    }
+}
--- Laurus.Mileage/Laurus.Mileage.Droid/Services/FileHelper.cs
+++ Laurus.Mileage/Laurus.Mileage.Droid/Services/FileHelper.cs
@@ -11,26 +11,54 @@ using Android.Views;
 using Android.Widget;
 using Xamarin.Forms;
 using Laurus.Mileage.Droid.Services;
+using Laurus.Mileage.Services;
+using System.IO;
+
 
 [assembly: Dependency(typeof(FileHelper))]
 namespace Laurus.Mileage.Droid.Services
 {
-   public class FileHelper : IFileHelper
-   {
-      public string GetLocalFilePath(string filename)
-      {
-         string path = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
-         return Path.Combine(path, filename);
-      }
+    public class FileHelper : IFileHelper
+    {
+        public string GetLocalFilePath(string filename)
+        {
+            string path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            return Path.Combine(path, filename);
+        }
 
-      public void Save(string filename, Stream stream)
-      {
-         throw new NotImplementedException();
-      }
+        public string Save(string filename, Stream stream)
+        {
+            //string docFolder = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            string docFolder = Forms.Context.ExternalCacheDir.AbsolutePath;
+            var filePath = Path.Combine(docFolder, filename);
+            FileStream fileStream = File.Open(filePath, FileMode.Create);
+            stream.Position = 0;
+            stream.CopyTo(fileStream);
+            fileStream.Flush();
+            fileStream.Close();
+            return filePath;
+        }
 
-      public Stream ReadTemplate()
-      {
-         throw new NotImplementedException();
-      }
-   }
+        public Stream ReadTemplate()
+        {
+            var filename = "template.xlsx";
+            var memStream = new MemoryStream();
+            using (StreamReader sr = new StreamReader(Forms.Context.Assets.Open(filename)))
+            {
+                sr.BaseStream.CopyTo(memStream);
+            }
+            memStream.Seek(0, SeekOrigin.Begin);
+            return memStream;
+        }
+
+        public Stream ReadFileContents(string fullPath)
+        {
+            if (File.Exists(fullPath))
+            {
+                return File.OpenRead(fullPath);
+            }
+            return null;
+        }
+
+    }
 }
